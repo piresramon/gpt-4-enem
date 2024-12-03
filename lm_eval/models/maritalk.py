126693b1482f05b0df19cbabeb472f9bb91b9880
Ramon Pires <pires.ramon@gmail.com>
2024-12-03 11:54:06 -0300

    Add MARITALKLM

---
+++ lm_eval/models/maritalk.py
@@ -0,0 +1,173 @@
+import json
+import openai
+import os
+import time
+from lm_eval.base import BaseLM
+from lm_eval import utils
+from tqdm import tqdm
+
+
+def get_result(response, ctxlen):
+    """Process results from OpenAI API response.
+
+    :param response: dict
+        OpenAI API Response
+    :param ctxlen: int
+        Length of context (so we can slice them away and only keep the predictions)
+    :return:
+        continuation_logprobs: np.array
+            Log probabilities of continuation tokens
+        is_greedy: bool
+            whether argmax matches given continuation exactly
+    """
+    is_greedy = True
+    logprobs = response["logprobs"]["token_logprobs"]
+    continuation_logprobs = sum(logprobs[ctxlen:])
+
+    for i in range(ctxlen, len(response["logprobs"]["tokens"])):
+        token = response["logprobs"]["tokens"][i]
+        top_tokens = response["logprobs"]["top_logprobs"][i]
+        top_token = max(top_tokens.keys(), key=lambda x: top_tokens[x])
+        if top_token != token:
+            is_greedy = False
+            break
+
+    return continuation_logprobs, is_greedy
+
+
+def oa_completion(**kwargs):
+    """Query OpenAI API for completion.
+
+    Retry with back-off until they respond
+    """
+    backoff_time = 3
+    while True:
+        try:
+            return openai.ChatCompletion.create(**kwargs)
+        except openai.error.OpenAIError:
+            import traceback
+
+            traceback.print_exc()
+            time.sleep(backoff_time)
+            backoff_time *= 1.5
+
+
+class MARITALKLM(BaseLM):
+    REQ_CHUNK_SIZE = 1
+
+    def __init__(self, engine, truncate=False):
+        """
+
+        :param engine: str
+            OpenAI API engine (e.g. davinci)
+        :param truncate: bool
+            Truncate input if too long (if False and input is too long, throw error)
+        """
+        super().__init__()
+
+        self.engine = engine
+
+        # Read from environment variable OPENAI_API_SECRET_KEY
+        openai.api_base = "https://chat.maritaca.ai/api"
+        openai.api_key = os.environ["MARITALK_API_SECRET_KEY"]
+
+    @property
+    def eot_token_id(self):
+        return self.tokenizer.eos_token_id
+
+    @property
+    def max_length(self):
+        # Note: the OpenAI API supports up to 2049 tokens, with the first token being the first input token
+        return 4000
+
+    @property
+    def max_gen_toks(self):
+        return 512
+
+    @property
+    def batch_size(self):
+        # Isn't used because we override _loglikelihood_tokens
+        raise NotImplementedError()
+
+    @property
+    def device(self):
+        # Isn't used because we override _loglikelihood_tokens
+        raise NotImplementedError()
+
+    def tok_encode(self, string: str):
+        return self.tokenizer.encode(string, add_special_tokens=False)
+
+    def tok_decode(self, tokens):
+        return self.tokenizer.decode(tokens)
+
+    def _loglikelihood_tokens(self, requests, disable_tqdm=False):
+        # ChatGPT does not suppport max_tokens=0 and does not return logprobs
+        raise NotImplementedError()
+
+    def greedy_until(self, requests):
+        if not requests:
+            return []
+        res = []
+
+        def _collate(x):
+            return len(x[0]), x[0]
+
+        re_ord = utils.Reorderer(requests, _collate)
+
+        def sameuntil_chunks(xs, size):
+            ret = []
+            lastuntil = xs[0][1]
+            for x in xs:
+                if len(ret) >= size or x[1] != lastuntil:
+                    yield ret, lastuntil
+                    ret = []
+                    lastuntil = x[1]
+                ret.append(x)
+
+            if ret:
+                yield ret, lastuntil
+
+        # todo: more intelligent batching for heterogeneous `until`
+        for chunk, until in tqdm(
+            list(sameuntil_chunks(re_ord.get_reordered(), self.REQ_CHUNK_SIZE))
+        ):
+            inps = []
+            for context, _ in chunk:
+                try:
+                    messages = json.loads(context)
+                except json.decoder.JSONDecodeError:
+                    # If context is not a valid JSON string, pass it as is
+                    messages = [{"role": "user", "content": context}]
+                inps.append(messages)
+
+            response = oa_completion(
+                model=self.engine,
+                messages=inps[0],
+                max_tokens=self.max_gen_toks, 
+                temperature=0.,
+                # stop=until,  # not working
+                ## The server had an error processing your request. Sorry about 
+                ## that! You can retry your request, or contact us through our 
+                ## help center at help.openai.com if you keep seeing this error.  
+            )
+
+            for resp, (context, until_) in zip(response.choices, chunk):
+                s = resp.message['content']
+
+                for term in until_:
+                    s = s.split(term)[0]
+
+                # partial caching
+                self.cache_hook.add_partial("greedy_until", (context, until_), s)
+
+                res.append(s)
+
+        return re_ord.get_original(res)
+
+    def _model_call(self, inps):
+        # Isn't used because we override _loglikelihood_tokens
+        raise NotImplementedError()
+
+    def _model_generate(self, context, max_length, eos_token_id):
+        # Isn't used because we override greedy_until
+        raise NotImplementedError()
